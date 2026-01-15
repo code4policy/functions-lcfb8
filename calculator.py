0cd2adcf579767581fb0484af1a1b342d1574287
lcfb8 <lydia.brosnahan@gmail.com>
2026-01-15 08:57:01 -0500

    added square, cube, and square n times to calculator

--- calculator.py
+++ calculator.py
@@ -10,7 +10,22 @@ def multiply(a,b):
 def divide(a,b):
 	return a/b
 
+def square(a):
+	return a*a
+
+def cube(a):
+	return a*a*a
+
+def square_n_times(a,n):
+	for n in range(n):
+		a = a*a
+	return a
+
 
 print ("I'm going use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
 print(x)
+
+print ("Test square n times, 2 squared 4 times")
+y = square_n_times(2,4)
+print(y)
